7ed54750986b8fad9ec1d68df1dd8b8440c32be1
Aniket Dhage <68240795+Aniketdhage@users.noreply.github.com>
2021-10-09 11:10:06 +0530

    last linked

--- jsFiles/jsCode.js
+++ jsFiles/jsCode.js
@@ -1,92 +1,54 @@
+let payButton = document.getElementById('pay')
 
-const btn = document.getElementById('btn');
-const textFeild = document.getElementById('input');
-textFeild.focus();
+payButton.addEventListener('click', function () {
+    let billAmount = document.getElementById('billAmount').value
+    let cashGiven = document.getElementById('cashGiven').value
 
-btn.addEventListener('click', () =>{
-    
-    if(textFeild.value){
-        const parent = document.getElementById('box');
-        const newElement = document.createElement('input');
-        
-        newElement.setAttribute("placeholder","Enter Paid Amount")
-        
-        newElement.classList.add('input');
-        
-        parent.appendChild(newElement).focus();
-        
-        const newButton = document.createElement('button');
-        newButton.innerHTML = 'PRESS';
-        newButton.classList.add('button');
-        parent.appendChild(newButton);
-         const oldEleParent = document.getElementById('main');
-         
-         const firstInputFeild = document.getElementById('input');
-         
-         const firstButtonParent = document.getElementById('span');
-     
-         oldEleParent.removeChild(firstButtonParent);
-        
-         oldEleParent.removeChild(firstInputFeild);
-      
+    let billAmountVal = document.getElementById('billAmountVal')
+    let cashGivenPrint = document.getElementById('cashGiven')
+    let cashToBeGivenPrint = document.getElementById('cashToBeGiven')
 
-        newButton.addEventListener('click',() => {
-            if(newElement.value){
-           
-            
-            parent.removeChild(newElement);
-            parent.removeChild(newButton);
-            
-            let MoneyReturn = +newElement.value - +textFeild.value;
-            const ReturnMoney = document.createElement('div');
-            if(MoneyReturn >= 0){
-                ReturnMoney.innerHTML =` Sucessful : ${MoneyReturn}Rs, Need to Return`;
-                ReturnMoney.classList.add('return');
+    let cashToBeReturned = cashGiven - billAmount
 
-
-                let changeDistribution = document.getElementById('change');
-                let Title = document.createElement('div');
-                Title.innerHTML = " CHANGE DISTRIBUTION"
-                changeDistribution.classList.add('title')
-                changeDistribution.appendChild(Title);
-                let distribution = document.createElement('div');
-    
-                let change = [2000,500,200,100,20,10,5,2,1];
-                let remainder =MoneyReturn;
-                let quotient = 0 ;
-                 for(let i=0;i<change.length;i++){
-                    quotient =   remainder / change[i];
-                    remainder = remainder%change[i];
-                    if(Math.floor(quotient) != 0 ){
-                    distribution.innerHTML += `${change[i]}Rs - ${Math.floor(quotient)} ,`;
-                    distribution.classList.add('dist');
-                    changeDistribution.appendChild(distribution);
-                    }
-                 }
-
-            }else{
-                ReturnMoney.innerHTML = " Failed : Paid amount cannot Be Less than Bill amount";
-                ReturnMoney.classList.add('Invalid');
-            }
-            parent.appendChild(ReturnMoney);
-
-            
-           
-               
-            
-            }else{
-                alert('enter a value first');
-            }
-        });
-    
+    let error = document.getElementById('error')
+    error.style.color = '#BA1C12'
+    if(billAmount == ''){
+        error.innerHTML = 'Please Enter the Bill Amount';
+    }else if(cashGiven == ''){
+        error.innerHTML = 'Please Enter the Cash Given'
+    }else if(cashToBeReturned < 0){
+        error.innerHTML = 'Please Pay More to Reach the Bill Amount, <br>' + '<b>' +Math.abs(cashToBeReturned)+'</b>' + ' more needed'
     }else{
-        alert('enter a value first')
+        error.innerHTML = ''
+
+    let notesPrint = document.getElementById('notes')
+    let cashHeading = document.getElementById('cashInterface')
+    let cashInterfaceStyle = document.getElementById('cashier')
+    cashHeading.innerHTML = 'Cashier Interface'
+    cashInterfaceStyle.style.padding = '28px 50px 50px 50px'
+    cashInterfaceStyle.setAttribute('class', 'animate__animated animate__fadeIn')
+
+    
+        billAmountVal.innerText = `Total Bill Amount : ${billAmount}`
+        cashGivenPrint.innerText = `Cash Given Amount : ${cashGiven}`
+            cashToBeGivenPrint.innerText = `Cash to be Returned Amount : ${cashToBeReturned}`
+
+    let cashDenomination = [ 2000, 500, 200, 100, 50, 20, 10, 5, 1 ];
+    let noteCounter = Array(9).fill(0);
+    
+    for (let i = 0; i < 9; i++) {
+        if (cashToBeReturned >= cashDenomination[i]) {
+            noteCounter[i] = Math.floor(cashToBeReturned / cashDenomination[i]);
+            cashToBeReturned = cashToBeReturned - noteCounter[i] * cashDenomination[i];
+        }
     }
-});
-
-
-
-
-
-
-
+    
+    noteList = ''
+    for (let i = 0; i < 9; i++) {
+        if (noteCounter[i] != 0) {
+        noteList +=  `${cashDenomination[i]}rs : ${noteCounter[i]} Notes <br> ` ;
+        }
+    }
+    notesPrint.innerHTML =`The Total Amount is Been Returned in <br> <b> ${noteList} </b>` ;
+    }
+})
